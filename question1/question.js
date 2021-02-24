ffb09ff3eb44228379ba873ab5ecd8d59e1e8db0
Nan Yuan <nan.yuan.dev@gmail.com>
2021-02-24 22:32:10 +1100

    Question1 build getSortedNumberList function

--- question1/question.js
+++ question1/question.js
@@ -1,6 +1,19 @@
 
-const date = [1, "2", true, false, "A", 123, undefined, null, "22", 6, 1, 67, { value: "A" }]
+const data = [1, "2", true, false, "A", 123, undefined, null, "22", 6, 1, 67, { value: "A" }]
 
-// see readme.md for information
+const getSortedNumberList = (data) => {
+  const filterdList = data.filter((item) => {
+    const isNumber = typeof item === "number";
+    const isNumberStr =
+      typeof item === "string" && item !== "" && !isNaN(Number(item));
+    return isNumber || isNumberStr;
+  });
 
-console.log("Done");
+  const sortedNumList = filterdList
+    .map((item) => Number(item))
+    .sort((a, b) => a - b);
+
+  return sortedNumList;
+};
+
+console.log(getSortedNumberList(data));
